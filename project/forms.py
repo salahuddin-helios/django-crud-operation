893158091fe3012175ac282ff23c6a1d2b628184
salahuddin8544 <salahuddinahmed8544@gmail.com>
2023-10-07 01:07:38 +0600

    crud operation

---
+++ project/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import GeeksModel
+
+class GeeksForm(forms.ModelForm):
+
+    class Meta:
+        model = GeeksModel
+        
+        fields = [
+            'name',
+            'number'
+
+        ]
